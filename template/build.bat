63f3a98833ff745e050859b7507f65e9b5b3c80b
Nika Fast <fastnika@yandex.ru>
2025-08-31 23:53:03 +0300

    correct scripts

--- template/build.bat
+++ template/build.bat
@@ -1,4 +1,22 @@
+rem
+rem Конфигурация скрипта для OC Windows
+rem
+
+rem Выставление кодировки UTF8
 chcp 65001
-del /S /Q out
-cmake -B out .
-cmake --build out
+
+rem
+rem Команды скрипта для OC Windows
+rem
+
+rem Директория построения
+set BUILDDIR=out
+
+rem Очистка директории построения при наличии
+if exist %BUILDDIR% del /S /Q %BUILDDIR%
+
+rem Формирование проекта
+cmake -B %BUILDDIR% .
+
+rem Построение проекта
+cmake --build %BUILDDIR%
